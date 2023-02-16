fe59e97542fcc51ba5a4d0687056a36d040be220
dusank01 <dusankostic01@gmail.com>
2023-02-16 18:18:40 +0100

    KRAJJJJJJJJJJJJJ

--- js/script.js
+++ js/script.js
@@ -18,6 +18,48 @@ $(document).ready(function(){
         $(".mapa").toggleClass("mapashowme");
         
       });
-      });
+    });
 
 
+    function myFunction() {
+      let ime = document.getElementById("imeprez").value;
+      let mail=document.getElementById("mail").value;
+      let telefon=document.getElementById("telefon").value;
+      let model=document.getElementById("model").value;
+      let napomena=document.getElementById("napomena").value;
+      if (ime=="") {
+        alert("Unesite ime i prezime");
+      } 
+      else if (mail=="") {
+        alert("Unesite imejl");
+      } 
+      else if (telefon=="") {
+        alert("Unesite telefon");
+      } 
+      else if(isNaN(telefon)){
+        alert("Nepravilan unos telefona");
+      }
+      else if (model=="") {
+        alert("Unesite model");
+      } 
+      else if (napomena=="") {
+        alert("Unesite napomenu");
+      } 
+      else{
+      alert("\nIme prezime:" + ime + "\nImejl:" + mail + "\nTelefon:" + telefon + "\nModel:" + model + "\nVaš problem:\n" + napomena + "\n\nProverite unete podatke!");
+      
+      }
+      document.getElementById("imeprez").value="";
+      document.getElementById("mail").value="";
+      document.getElementById("telefon").value="";
+      document.getElementById("model").value="";
+      document.getElementById("napomena").value="";
+      alert("Vaša kontakt forma je primljena. Očekujte odgovor u najkraćem roku!\nHvala na ukazanom poverenju")
+    }
+
+
+  
+
+
+
+  
